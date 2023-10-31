b4214d179ff3f1ba093c634937a6ce800182c6f5
dawid-ciborowski-vinci <dawid.ciborowski@student.vinci.be>
2023-10-31 12:20:09 +0100

    refactor: greet return Hello, my friend

--- app.js
+++ app.js
@@ -1,10 +1,6 @@
 function greet(name) {
-    if (name === null)
-        return "Hello, my friend.";
-    if (name === undefined)
-        return "Hello, my friend.";
-    if (name.trim() === "")
-        return "Hello, my friend.";
-    return `Hello, ${name}.`;
+  if (name === null || name === undefined || name.trim() === "")
+    return "Hello, my friend.";
+  return `Hello, ${name}.`;
 }
 module.exports = greet;
