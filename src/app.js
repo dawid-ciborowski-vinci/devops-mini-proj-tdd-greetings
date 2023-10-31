ffdbd6045104da04f969be8714e9497614913e70
ahmetkusay-demir-vinci <128645290+ahmetkusay-demir-vinci@users.noreply.github.com>
2023-10-31 17:31:48 +0100

    refactor: app.js and app.test.js

--- src/app.js
+++ src/app.js
@@ -1,95 +1,116 @@
+const languages = {
+  en: {
+    incorrect: "Hello, my friend.",
+    upperCase: "HELLO,",
+    lowerCase: "Hello,",
+    and: "and",
+    andUpper: "AND",
+  },
+  fr: {
+    incorrect: "Bonjour, mon ami.",
+    upperCase: "BONJOUR,",
+    lowerCase: "Bonjour,",
+    and: "et",
+    andUpper: "ET",
+  },
+  nl: {
+    incorrect: "Hallo, mijn vriend.",
+    upperCase: "HALLO,",
+    lowerCase: "Hallo,",
+    and: "en",
+    andUpper: "EN",
+  },
+};
+
 function greet(names, language = "en") {
-  if (language != "en" && language != "fr" && language != "nl") {
+  if (!isLanguageSupported(language)) {
     return "This language is not supported.";
   }
-  const languages = {
-    en: {
-      incorrect: "Hello, my friend.",
-      upperCase: "HELLO,",
-      lowerCase: "Hello,",
-      and: "and",
-      andUpper: "AND",
-    },
-    fr: {
-      incorrect: "Bonjour, mon ami.",
-      upperCase: "BONJOUR,",
-      lowerCase: "Bonjour,",
-      and: "et",
-      andUpper: "ET",
-    },
-    nl: {
-      incorrect: "Hallo, mijn vriend.",
-      upperCase: "HALLO,",
-      lowerCase: "Hallo,",
-      and: "en",
-      andUpper: "EN",
-    },
-  };
 
-  const selectedLanguage = languages[language] || languages["en"];
+  const selectedLanguage = languages[language];
 
-  if (isIncorrect(names)) {
+  if (isNameIncorrect(names)) {
     return selectedLanguage.incorrect;
   }
 
-  if (typeof names === "string") {
-    if (isUpperCase(names)) {
-      return `${selectedLanguage.upperCase} ${names}!`;
-    } else {
-      return `${selectedLanguage.lowerCase} ${names}.`;
-    }
-  } else if (Array.isArray(names) && names.length >= 2) {
-    const andTranslation = selectedLanguage.and;
-    const lowerCaseTranslation = selectedLanguage.lowerCase;
-    const upperCaseTranslation = selectedLanguage.upperCase;
-    const andUpperTranslation = selectedLanguage.andUpper;
-    let lowercaseNames = [];
-    let uppercaseNames = [];
-
-    names.forEach((name) => {
-      if (isUpperCase(name)) {
-        uppercaseNames.push(name);
-      } else {
-        lowercaseNames.push(name);
-      }
-    });
-
-    if (lowercaseNames.length !== 0 && uppercaseNames.length !== 0) {
-      return (
-        namesToString(lowercaseNames, andTranslation, lowerCaseTranslation) + ". " +
-        uppercaseNamesToString(uppercaseNames, upperCaseTranslation, andUpperTranslation)
-      );
-    }
-    if(lowercaseNames.length !== 0){
-      return namesToString(lowercaseNames, andTranslation, lowerCaseTranslation)+".";
-    }
-    return namesToString(uppercaseNames, andUpperTranslation, upperCaseTranslation)+"!";
+  if (isSingleName(names)) {
+    return formatSingleNameGreeting(names, selectedLanguage);
+  } else if (isMultipleNames(names)) {
+    return formatMultipleNamesGreeting(names, selectedLanguage);
   }
 
   return `Hello, ${names}.`;
 }
 
-function namesToString(names, and, lowerCase) {
-  return `${lowerCase} ${names.slice(0, -1).join(", ")} ${and} ${
-    names[names.length - 1]
-  }`;
+function isLanguageSupported(language) {
+  return languages[language] !== undefined;
 }
 
-function uppercaseNamesToString(names, upperCase, andUpper) {
-  return `${andUpper} ${upperCase} ${names[0]}!`;
-}
-
-function isUpperCase(name) {
-  return name === name.toUpperCase();
-}
-
-function isIncorrect(name) {
+function isNameIncorrect(name) {
   if (typeof name === "string") {
     return !name || name.trim() === "";
   } else if (Array.isArray(name)) {
     return name.length === 0;
   }
   return true;
+}
+
+function isSingleName(name) {
+  return typeof name === "string";
+}
+
+function isMultipleNames(names) {
+  return Array.isArray(names) && names.length >= 2;
+}
+
+function formatSingleNameGreeting(name, selectedLanguage) {
+  if (isNameUppercase(name)) {
+    return `${selectedLanguage.upperCase} ${name}!`;
+  } else {
+    return `${selectedLanguage.lowerCase} ${name}.`;
+  }
+}
+
+function formatMultipleNamesGreeting(names, selectedLanguage) {
+  const { and, lowerCase, upperCase, andUpper } = selectedLanguage;
+  const lowercaseNames = [];
+  const uppercaseNames = [];
+
+  names.forEach((name) => {
+    if (isNameUppercase(name)) {
+      uppercaseNames.push(name);
+    } else {
+      lowercaseNames.push(name);
+    }
+  });
+
+  if (lowercaseNames.length !== 0 && uppercaseNames.length !== 0) {
+    return (
+      joinNames(lowercaseNames, and, lowerCase) +
+      ". " +
+      joinUppercaseNames(uppercaseNames, upperCase, andUpper)
+    );
+  }
+
+  if (lowercaseNames.length !== 0) {
+    return joinNames(lowercaseNames, and, lowerCase) + ".";
+  }
+
+  return joinNames(uppercaseNames, andUpper, upperCase) + "!";
+}
+
+function joinNames(names, and, caseType) {
+  return `${caseType} ${names.slice(0, -1).join(", ")} ${and} ${
+    names[names.length - 1]
+  }`;
+}
+
+function joinUppercaseNames(names, upperCase, andUpper) {
+  return `${andUpper} ${upperCase} ${names[0]}!`;
+}
+
+function isNameUppercase(name) {
+  return name === name.toUpperCase();
 }
 
 module.exports = greet;
