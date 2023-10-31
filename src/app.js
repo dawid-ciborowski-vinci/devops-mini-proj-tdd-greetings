3f93a9cbc5c66464e405afa3c8eb2e3d8b896ed9
dawid-ciborowski-vinci <dawid.ciborowski@student.vinci.be>
2023-10-31 14:33:25 +0100

    refactor: app.js

--- src/app.js
+++ src/app.js
@@ -1,9 +1,19 @@
 function greet(names) {
-  if (Array.isArray(names) && names.length == 2)
-    return `Hello, ${names[0]} and ${names[1]}.`;
-  if (names === null || names === undefined || names.trim() === "")
-    return "Hello, my friend.";
-  if (names === names.toUpperCase()) return `HELLO, ${names}!`;
+  if (twoNames(names)) return `Hello, ${names[0]} and ${names[1]}.`;
+  if (isIncorrect(names)) return "Hello, my friend.";
+  if (isUpperCase(names)) return `HELLO, ${names}!`;
   return `Hello, ${names}.`;
+}
+function twoNames(names) {
+  if (Array.isArray(names) && names.length == 2) return true;
+  return false;
+}
+function isIncorrect(name) {
+  if (names === null || names === undefined || names.trim() === "") return true;
+  return false;
+}
+function isUpperCase(name) {
+  if (names === names.toUpperCase()) return true;
+  return false;
 }
 module.exports = greet;
