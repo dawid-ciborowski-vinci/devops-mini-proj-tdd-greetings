2b58e0da1054fc9585113ed4832cf3e499498487
Nicolae-Pastrama-vinci <nicolae.pastrama@student.vinci.be>
2023-10-31 13:00:44 +0100

    refactor: return uppercase name

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ function greet(name) {
   if (name === null || name === undefined || name.trim() === "")
     return "Hello, my friend.";
   if (name === name.toUpperCase())
-    return "HELLO, " + name + "!";
+    return `HELLO, ${name}!`;
   return `Hello, ${name}.`;
 }
 module.exports = greet;
