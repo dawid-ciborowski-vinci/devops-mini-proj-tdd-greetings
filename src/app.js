5f3974f30b56b322a1fc1ecd87a1bfba595297bb
ahmetkusay-demir-vinci <128645290+ahmetkusay-demir-vinci@users.noreply.github.com>
2023-10-31 13:41:17 +0100

    reformat: code with prettier

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 function greet(names) {
-  if (Array.isArray(names) && names.length == 2) return `Hello, ${names[0]} and ${names[1]}.`;
+  if (Array.isArray(names) && names.length == 2)
+    return `Hello, ${names[0]} and ${names[1]}.`;
   if (names === null || names === undefined || names.trim() === "")
     return "Hello, my friend.";
   if (names === names.toUpperCase()) return `HELLO, ${names}!`;
